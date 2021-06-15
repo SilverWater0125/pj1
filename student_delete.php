900cd71e6a49c351962b5944db57e4bedb3f7f99
leeeunsoo056 <81094167+leeeunsoo056@users.noreply.github.com>
2021-06-15 17:52:56 +0900

    Update student_delete.php

--- student_delete.php
+++ student_delete.php
@@ -8,7 +8,7 @@
  </head> 
 <body> 
      <h1> 탈퇴 </h1> 
-     <FORM METHOD="get" ACTION="student_delete_result.php"> 
+     <FORM METHOD="post" ACTION="student_delete_result.php"> 
          아이디 : <INPUT TYPE="text" NAME="아이디"> <BR><br>
 	탈퇴하시면 이용중인 서비스가 종료되며, 모든 데이터는 복구 불가능합니다.<br><br>
          <INPUT TYPE="submit" VALUE="탈퇴하기"> 
