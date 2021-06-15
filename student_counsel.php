8553348e4726380ea0007f0e2e1636589e54efd8
leeeunsoo056 <81094167+leeeunsoo056@users.noreply.github.com>
2021-06-16 03:36:34 +0900

    Update student_counsel.php

--- student_counsel.php
+++ student_counsel.php
@@ -11,6 +11,18 @@ if($ret){
         exit();
         }
      }
+else {
+      echo "데이터 조회에 실패했습니다"."<br>";
+      echo "실패 원인:".mysqli_error($con);
+      echo "<br> <a href='student.html'> <---뒤로가기 </a> ";
+      exit();
+}
+$row= mysqli_fetch_array($ret);
+$학생아이디 = $row['아이디'];
+$학원이름 = $row["학원이름"];
+$상담날짜= $row["상담날짜"];
+$상담내용 = $row["상담내용"];
+?>
 
 ?>
 <html>
@@ -23,10 +35,10 @@ if($ret){
  <body> 
      <h1>상담 신청</h1> 
      <FORM METHOD="post" ACTION="student_counsel_result.php"> 
-         아이디 : <INPUT TYPE="text" NAME="학생아이디"><br> 
-         학원이름 : <INPUT TYPE="text" NAME="학원이름"><br>
-         상담날짜 : <INPUT TYPE="text" NAME="상담날짜"><br> 
-        상담내용: <INPUT TYPE="text" NAME="상담내용"><br> 
+         아이디 : <INPUT TYPE="text" NAME="학생아이디" VALUE=<?php echo $학생아이디 ?> READONLY> <br> 
+         학원이름 : <INPUT TYPE="text" NAME="학원이름" VALUE=<?php echo $학원이름 ?> READONLY> <br> 
+         상담날짜 : <INPUT TYPE="text" NAME="상담날짜" VALUE=<?php echo $상담날짜 ?> READONLY> <br> 
+        상담내용: <INPUT TYPE="text" NAME="상담내용" VALUE=<?php echo $상담내용 ?> READONLY> <br> 
         
          <br><br> 
          <INPUT TYPE="submit" value="상담 신청하기"> 
