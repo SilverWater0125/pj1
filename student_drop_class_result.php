2968f88d83c7e29f01416f158fd9a5526578df1b
leeeunsoo056 <81094167+leeeunsoo056@users.noreply.github.com>
2021-06-15 18:59:25 +0900

    Add files via upload

---
+++ student_drop_class_result.php
@@ -0,0 +1,22 @@
+<?php 
+    $con=mysqli_connect("localhost", "root", "password", "학원관리시스템") or die("MySQL 접속에 실패하셨습니다"); 
+    $userID = $_POST["아이디"];
+    $sql ="DELETE FROM 수강 WHERE 아이디='".$아이디."'";
+ 
+ 
+    $ret = mysqli_query($con, $sql);    
+    echo "<h1> 수강 취소하기 </h1>";
+    if($ret) { 
+ 	echo  " 회원님의 수강취소가 완료되었습니다.";
+ 	   }		    
+    
+    else { 
+ 	   echo "수강취소에 실패하였습니다"."<br>"; 
+ 	   echo "실패 원인 :".mysqli_error($con); 
+ 	   exit(); 
+    }    
+    mysqli_close($con);
+    
+    echo "<br><br> <a href = 'student.html'>  <---초기화면으로 돌아가기</a>";
+      
+?> 
---
+++ student_insert.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html> 
+ <html lang="en"> 
+ <head>
+     <meta charset="UTF-8"> 
+     <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+     <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+     <title>Document</title> 
+ </head> 
+ <body> 
+     <h1>회원가입</h1> 
+     <FORM METHOD="post" ACTION="student_insert_result.php"> 
+         아이디 : <INPUT TYPE="text" NAME="아이디"><br> 
+         비밀번호 : <INPUT TYPE="text" NAME="비밀번호"><br>
+         이름 : <INPUT TYPE="text" NAME="이름"><br> 
+        나이 : <INPUT TYPE="text" NAME="나이"><br> 
+         학교 : <INPUT TYPE="text" NAME="학교"><br> 
+        연락처 : <INPUT TYPE="text" NAME="학교"><br> 
+        
+         <br><br> 
+         <INPUT TYPE="submit" value="등록"> 
+     </FORM> 
+ </body> 
+ </html> 
