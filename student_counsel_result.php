2cc307a58a51968fe026769e13b47df24751abdd
leeeunsoo056 <81094167+leeeunsoo056@users.noreply.github.com>
2021-06-17 21:33:56 +0900

    Add files via upload

---
+++ student_counsel_result.php
@@ -0,0 +1,31 @@
+<?php 
+    $con=mysqli_connect("localhost", "root", "password", "학원관리시스템") or die("MySQL 접속 실패에 실패하였습니다"); 
+ 
+ 
+    $아이디 = $_POST["아이디"]; 
+    $학원이름= $_POST["학원이름"]; 
+    $상담날짜 = $_POST["상담날짜"]; 
+    $상담내용 = $_POST["상담내용"]; 
+
+
+      $first = " SELECT 아이디 FROM 학생 WHERE 아이디 = '".$_GET['학생아이디']."'";
+      $second = " SELECT 이름 FROM 학원 WHERE 이름= '".$_GET['학원이름']."'";
+      $sql =" INSERT INTO 관리 VALUES('".$first."','".$second."','".$상담날짜."','".$상담내용."')";
+    
+   
+    $ret = mysqli_query($con, $sql); 
+   
+     echo "<h1> 상담신청 </h1>"; 
+    if($ret) { 
+ 	   echo "상담신청이 성공적으로 완료되었습니다.."; 
+    } 
+    else {
+ 	   echo "상담신청에 실패하였습니다"."<br>"; 
+	   echo" 다음과 같은 사항들을 점검해보십시요."."<br>";
+	
+ 	   echo "실패 원인 :".mysqli_error($con); 
+    }  
+    mysqli_close($con); 
+     
+    echo "<br> <a href='student.html'> <--초기 화면</a> "; 
+?> 
