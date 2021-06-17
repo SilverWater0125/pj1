6698d48f9e5905aaa880d996dcd9dbee9e484d41
leeeunsoo056 <81094167+leeeunsoo056@users.noreply.github.com>
2021-06-17 19:56:51 +0900

    Add files via upload

---
+++ student_counsel.php
@@ -0,0 +1,17 @@
+<HTML>
+ <head>
+ <META http-equiv="content-type" content="text/html; charset=utf-8">
+ </head> 
+ <body> 
+     <h1>상담 신청</h1> 
+     <FORM METHOD="post" ACTION="student_counsel_result.php"> 
+         아이디 : <INPUT TYPE="text" NAME="학생아이디"><br> 
+         학원전화번호 : <INPUT TYPE="text" NAME="학원전화번호"><br>
+         상담날짜 : <INPUT TYPE="text" NAME="상담날짜"><br> 
+        상담내용: <INPUT TYPE="text" NAME="상담내용"><br> 
+        
+         <br><br> 
+         <INPUT TYPE="submit" value="상담 신청하기"> 
+     </FORM> 
+ </body> 
+ </html> 
---
+++ student_take_class.php
@@ -0,0 +1,16 @@
+<HTML>
+ <head>
+<META http-equiv="content-type" content="text/html; charset=utf-8">
+ </head> 
+ <body> 
+     <h1> 수강 신청 </h1> 
+     <FORM METHOD="post" ACTION="student_take_class_result.php"> 
+         아이디: <INPUT TYPE="text" NAME="학생아이디"><br> 
+         과목번호 : <INPUT TYPE="text" NAME="과목번호"><br>
+ 
+         <br><br> 
+         <INPUT TYPE="submit" value="수강 신청하기"> 
+
+     </FORM> 
+ </body> 
+ </HTML> 
