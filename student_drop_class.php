53b227f95e9b4378b7f80a802525ce0ce2226f83
leeeunsoo056 <81094167+leeeunsoo056@users.noreply.github.com>
2021-06-17 21:39:14 +0900

    Update student_drop_class.php

--- student_drop_class.php
+++ student_drop_class.php
@@ -12,7 +12,7 @@
       
      <FORM METHOD="post" ACTION="student_drop_class_result.php"> 
          아이디 : <INPUT TYPE="text" NAME="아이디"> <BR><br>
-	수강을 취소하시겠습니까?<br><br>
+	 수강을 취소하시겠습니까?<br><br>
          <INPUT TYPE="submit" VALUE="취소하기"> 
      </FORM> 
  </body> 
