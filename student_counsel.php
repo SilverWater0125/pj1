64093edcdd911d8add689782538aab98e0b4b45f
leeeunsoo056 <81094167+leeeunsoo056@users.noreply.github.com>
2021-06-17 21:32:45 +0900

    Update student_counsel.php

--- student_counsel.php
+++ student_counsel.php
@@ -8,7 +8,7 @@
          아이디 : <INPUT TYPE="text" NAME="학생아이디"><br> 
          학원이름 : <INPUT TYPE="text" NAME="학원이름"><br>
          상담날짜 : <INPUT TYPE="text" NAME="상담날짜"><br> 
-        상담내용: <INPUT TYPE="text" NAME="상담내용"><br> 
+         상담내용: <INPUT TYPE="text" NAME="상담내용"><br> 
         
          <br><br> 
          <INPUT TYPE="submit" value="상담 신청하기"> 
